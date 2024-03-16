831a4cc1a6013190ffafdfbbd80156dcd8b47189
Suvodip Chakraborty <chakrabortysuvodip3@gmail.com>
2024-03-17 02:46:17 +0530

    minor changes

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -63,7 +63,7 @@ export default function Home() {
               <Chip
                 variant="shadow"
                 classNames={{
-                  base: "bg-gradient-to-br from-indigo-500 to-pink-500 border-small border-white/50 shadow-pink-500/30 p-8 text-2xl ",
+                  base: "bg-gradient-to-br from-indigo-500 to-pink-500 border-small border-white/50 shadow-pink-500/30 p-8 text-2xl px-20",
                   content: "drop-shadow shadow-black text-whiteanimate-pulse",
                 }}
               >
